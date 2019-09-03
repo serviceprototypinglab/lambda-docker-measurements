3303fce25e5ecd50fb80ef8ba472df0715c469cb
Dai Boruta <boru@zhaw.ch>
2019-09-03 16:47:06 +0200

    Added csv file and timestamps for future plots

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Docker measurements
 
 Measurements of time and memory costs for docker containers
+
+Running an example (it takes a while)
+docker run --name benchmark unimarijo/linux-benchmarks & ./stats.sh benchmark
--- stats.sh
+++ stats.sh
@@ -5,23 +5,25 @@ CONTAINER=$1
 sleep 0.1
 #Empty the stats aux file
 echo "" > aux
+FILE="$CONTAINER-rawresults".csv
+echo "" > $FILE
 #Obtain max memory usage
 ##get container's current status
 status="$(docker inspect --format '{{.State.Status}}' $CONTAINER)"
-echo "$status"
+##echo "$status"
 ##wait for container to be running
 while [ $status != "running" ]
 do
     sleep 0.1
     status="$(docker inspect --format '{{.State.Status}}' $CONTAINER)"
-    echo "$status"
 done
-echo "$status"
 ##loop: while the container is running, get the current memory usage into the aux file
 while [ $status != "exited" ]
 do
-    sleep 0.1
-    docker stats --no-stream --format "{{.MemUsage}}" $CONTAINER >> aux
+    sleep 0.02
+    mem="$(docker stats --no-stream --format "{{.MemUsage}}" $CONTAINER)"
+    echo ${mem%/*} >> aux
+    echo "$(date +"%T.%3N"),${mem%/*}" >> $FILE
     status="$(docker inspect --format '{{.State.Status}}' $CONTAINER)"
 done
 ##sort memory usage file and get the highest value
