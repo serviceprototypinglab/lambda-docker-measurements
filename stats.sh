acc14e56f37d25421bec1308de7d275147fd5b74
Dai Boruta <boru@zhaw.ch>
2019-09-04 10:32:13 +0200

    Modified csv output to be the raw bytes value of memory usage

--- stats.sh
+++ stats.sh
@@ -14,11 +14,11 @@ sleep 0.1
 echo "" > aux
 FILE="$CONTAINER-rawresults".csv
 echo "" > $FILE
+
 #Obtain max memory usage
 ##get container's current status
 status="$(docker inspect --format '{{.State.Status}}' $CONTAINER)"
-##echo "$status"
-
+CONTAINERID="$(docker inspect --format '{{.Id}}' $CONTAINER)"
 if [ "$status" = "" ]
 then
     echo "Container '$CONTAINER' not found!"
@@ -31,19 +31,24 @@ do
     sleep 0.001
     status="$(docker inspect --format '{{.State.Status}}' $CONTAINER)"
 done
+
 ##loop: while the container is running, get the current memory usage into the aux file
 while [ $status != "exited" ]
 do
     sleep 0.001
-    mem="$(docker stats --no-stream --format "{{.MemUsage}}" $CONTAINER)"
-    echo ${mem%/*} >> aux
-    echo "$(date +"%T.%3N"),${mem%/*}" >> $FILE
+    ##mem="$(docker stats --no-stream --format "{{.MemUsage}}" $CONTAINER)"
+    mem="$(cat /sys/fs/cgroup/memory/docker/$CONTAINERID/memory.usage_in_bytes)"
+    echo $mem >> aux
+    echo "$(date +"%T.%3N"),$mem" >> $FILE
     status="$(docker inspect --format '{{.State.Status}}' $CONTAINER)"
 done
+
 ##sort memory usage file and get the highest value
 MEMORY=$(sort -k 1 -h aux | tail -n 1)
+
 ##print found value
 echo 'max memory usage:' "$MEMORY"
+
 #Obtain runtime in ms
 START=$(date --date=$(docker inspect --format='{{.State.StartedAt}}' $CONTAINER) +%s%3N)
 STOP=$(date --date=$(docker inspect --format='{{.State.FinishedAt}}' $CONTAINER) +%s%3N)
